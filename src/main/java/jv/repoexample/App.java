d86a13ed26ad8fe6c5666a7ecd7a7c89e4d2db01
stan-o <orlov.stanislav.e@gmail.com>
2017-01-22 11:37:56 +0200

    modify App

--- src/main/java/jv/repoexample/App.java
+++ src/main/java/jv/repoexample/App.java
@@ -2,6 +2,6 @@ package jv.repoexample;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello vasja");
+        System.out.println("hello vasja !!!!!!!");
     }
 }
